61e7db00a9db14ea2bcd32cc8782d56275231389
Yancy <yancy@symph.co>
2023-05-27 22:20:03 +0800

    -Added animations to services section cards

--- tupas_portfolio/src/components/Layout/index.js
+++ tupas_portfolio/src/components/Layout/index.js
@@ -9,6 +9,20 @@ import Sticker_1 from '../../assets/Sticker_1.png'
 import Sticker_3 from '../../assets/Sticker_3.png'
 import Sticker_4 from '../../assets/Sticker_4.png'
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })     
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 const Layout = () => {
     return (
         <>
@@ -73,7 +87,7 @@ const Layout = () => {
                         <div className='services_container'>
 
                             <div className='brand_container'>
-                                <div className='brand_details' data-tilt>
+                                <div className='brand_details hidden'>
                                     <img src={Sticker_3} alt="Sticker_3" className="Sticker_2"/>
                                     <h1>Brand Design</h1>  
                                     <p> Crafting impactful brand identities that capture the essence of your business. From logo design to brand guidelines, I create visual assets that resonate with your target audience and convey your unique story. </p>
@@ -81,7 +95,7 @@ const Layout = () => {
                             </div>
 
                             <div className='fe_dev'> 
-                                <div className='fe_details'>
+                                <div className='fe_details hidden'>
                                     <img src={Sticker_1} alt="Sticker_1" className="Sticker_1"/>
                                     <h1>Frontend Development</h1>                       
                                     <p>Transforming designs into seamless user experiences through clean and efficient code. I specialize in creating responsive websites and web applications that not only look stunning but also provide a smooth and intuitive interface for users.</p>
@@ -89,7 +103,7 @@ const Layout = () => {
                             </div>
 
                             <div className='ui_design'>
-                                <div className='ui_details'>
+                                <div className='ui_details hidden'>
                                     <img src={Sticker_4} alt="Sticker_4"className="Sticker_3" />
                                     <h1>UI/UX Design</h1>
                                    <p>Designing user-centered interfaces that enhance the overall user experience. From wireframes to interactive prototypes, I create visually appealing and functional designs that prioritize usability, accessibility, and engagement, resulting in a delightful user journey.</p> 
